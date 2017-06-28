8b1fb0218827098f5e7ac61cbb7b5f6c96fa75ca
Empress <1321745454@qq.com>
2017-06-28 23:20:05 +0800

    实现了从闪屏界面跳转到主界面

--- app/src/main/java/cn/software_engineering/jkbdbyempress/splashActivity.java
+++ app/src/main/java/cn/software_engineering/jkbdbyempress/splashActivity.java
@@ -1,5 +1,6 @@
 package cn.software_engineering.jkbdbyempress;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -15,5 +16,7 @@ public class splashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        Intent intent=new Intent(splashActivity.this,MainActivity.class);
+        startActivity(intent);
     }
 }
