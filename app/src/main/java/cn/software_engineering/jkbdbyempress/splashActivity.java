97811192f1e56d3b05130758230fbb9c13cb2c1b
EmpressBoost <1321745454@qq.com>
2017-06-28 15:33:03 +0800

    修改了32位内存过小导致编译慢的问题

---
+++ app/src/main/java/cn/software_engineering/jkbdbyempress/splashActivity.java
@@ -0,0 +1,19 @@
+package cn.software_engineering.jkbdbyempress;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.software_engineering.jkbdbyempress.R;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+
+public class splashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
